5095eeb88e8bf99bfbb7d6fffc3ad6ee0b50d76f
Mike Zangus <zangusm@gmail.com>
2023-08-12 20:46:19 -0400

    update test file

--- neural_networks/characters_test.py
+++ neural_networks/characters_test.py
@@ -13,7 +13,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 data_path = Path("data/by_character")
 weights_path = Path("neural_networks/weights")
 
-input_letter = "Z"
+input_character = input("What character do you want to test? ")
+if input_character.isalpha():
+    input_character_type = input("Cap or low? ")
+else:
+    input_character_type = "num"
 
 transform = transforms.Compose([
     transforms.Grayscale(num_output_channels = 1),
@@ -43,12 +47,12 @@ def test(image_path, model):
         output = model(image)
         _, predicted = torch.max(output.data, 1)
         if predicted.item() == 0:
-            return f"{input_letter}"
+            return f"{input_character}"
         else:
-            return f"Not {input_letter}"
+            return f"Not {input_character}"
 
 if __name__ == "__main__":
-    weights = weights_path / f"weights_{input_letter}_cap.pth"
+    weights = weights_path / f"weights_{input_character}_{input_character_type}.pth"
     model = NeuralNet().to(device)
     model.load_state_dict(torch.load(weights))
     model.eval()
@@ -57,24 +61,29 @@ if __name__ == "__main__":
     fig_width = grid_size[1] * (1 + spacing)
     fig_height = grid_size[0] * (1 + spacing)
     fig, ax = plt.subplots(grid_size[0], grid_size[1], figsize = (fig_width, fig_height))
-    letters = [chr(65 + i) for i in range(26)]
+    characters = [chr(65 + i) for i in range(26)] + [str(i) for i in range(10)]
+    character_types = ["cap", "low", "num"]
     random_image_paths = []
-    while len(random_image_paths) < grid_size[0] * grid_size[1]:
-        test_letter = random.choice(letters)
-        image_files = os.listdir(data_path / f"{test_letter}_cap")
+    for test in range(grid_size[0] * grid_size[1]):
+        test_character = random.choice(characters)
+        if test_character.isalpha():
+            test_character_type = random.choice(character_types[:2])
+        else:
+            test_character_type = character_types[2]
+        image_files = os.listdir(data_path / f"{test_character}_{test_character_type}")
         random_image_path = random.choice(image_files)
-        random_image_paths.append((test_letter, random_image_path))
-    for test, (test_letter, image_path) in enumerate(random_image_paths):
-        image = Image.open(data_path / f"{test_letter}_cap" / image_path).convert("L")
+        random_image_paths.append((test_character, test_character_type, random_image_path))
+    for test, (test_character, test_character_type, image_path) in enumerate(random_image_paths):
+        image = Image.open(data_path / f"{test_character}_{test_character_type}" / image_path).convert("L")
         image = transform(image).unsqueeze(0).to(device)
         predicted_value = torch.argmax(model(image)).item()
-        predicted_label = f"{input_letter}" if predicted_value == 0 else f"not {input_letter}"
+        predicted_label = f"{input_character} {input_character_type}" if predicted_value == 0 else f"not {input_character} {input_character_type}"
         label_color = "blue" if predicted_value == 0 else "gray"
         x_index = test % grid_size[1]
         y_index = test // grid_size[1]
         ax[y_index, x_index].imshow(transforms.ToPILImage()(image.squeeze()), cmap = "gray")
         ax[y_index, x_index].set_xticks([])
         ax[y_index, x_index].set_yticks([])
-        ax[y_index, x_index].set_title(f"Predicted: {predicted_label}\nActual: {test_letter}", color = label_color)
+        ax[y_index, x_index].set_title(f"Predicted: {predicted_label}\nActual: {test_character} {test_character_type}", color = label_color)
     plt.tight_layout()
     plt.show()
